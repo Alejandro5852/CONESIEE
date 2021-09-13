0e18ecf783880af514f0cb4c8388fe3570e2a315
César Alejandro Sosa Enríquez <60756401+Alejandro5852@users.noreply.github.com>
2021-09-13 16:38:56 -0600

    prueba react

--- conesieee/src/App.js
+++ conesieee/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Registro from './components/Registro/Registro'
+import Perfil from './components/Perfil/Perfil'
 
-function App() {
+/* function Helloworld(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="hello">
+      <h3>{props.subtitle}</h3>
+      {props.mytext}
     </div>
   );
-}
+} */
 
+/* class Helloworld extends React.Component {
+  state = {
+    show: true
+  }
+  toggleshow = ()=>{
+    this.setState({show : !this.state.show})
+  }
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          {this.props.mytext}
+          <button onClick = {this.toggleshow}
+          >Toggle show</button>
+        </div>
+      );
+    }else{
+      return <div id="hello"><h1>There are not elements</h1> <button onClick = {this.toggleshow}>Toggle show</button></div>
+    }
+  }
+}
+function App() {
+  return (
+    <div>
+      This is my component:
+      <Helloworld mytext="Hello Alejandro" subtitle="lorem ipsum" />
+      <Helloworld mytext="Hola Mundo" subtitle="component two" />
+      <Helloworld mytext="Hello!" subtitle="component three" />
+    </div>
+  );
+} */
+
+class App extends Component {
+  state = {
+    tasks: {}
+  }
+
+
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" render={() => 
+          {return <div className = "contenedor-perfil"><Perfil/></div>}
+        }>
+        </Route>
+        <Route exact path="/registro" render={() => 
+          {return <div className = "contenedor-registro"><Registro/></div>}
+        }>
+        </Route>       
+      </Router>);
+  }
+}
 export default App;
---
+++ conesieee/src/components/Perfil/Perfil.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Perfil.css'
+export default class Perfil extends Component{
+    
+
+    render(){
+        return(<h1>Si jala esto :v</h1>)
+    }
+}
+
+
