078cad68538f29d159d34045501f384638ea2913
César Alejandro Sosa Enríquez <60756401+Alejandro5852@users.noreply.github.com>
2021-09-13 20:55:33 -0600

    landing page finalizada

--- conesieee/src/App.js
+++ conesieee/src/App.js
@@ -47,15 +47,49 @@ function App() {
 } */
 
 class App extends Component {
+  /*
   state = {
-    tasks: {}
+    tasks: tasks
+  }
+  addTask = (title, description) => {
+    const newTask = {
+      title: title,
+      description: description,
+      id: this.state.tasks.length
+    }
+    this.setState({
+      tasks: [...this.state.tasks, newTask]
+    })
+  }
+  deleteTask = (id) => {
+    const newTasks = this.state.tasks.filter(task => task.id !== id);
+    this.setState({ tasks: newTasks });
+  }
+  checkDone = id => {
+    const newTasks = this.state.tasks.map(task => {
+      if (task.id === id) {
+        task.done = !task.done
+      }
+      return task;
+    });
+    this.setState({ tasks: newTasks });
   }
 
+
+              <html>
+            <head>
+                <title>CONESIEEE</title>
+            </head>
+            <body>
+            
+            </body>
+            </html>
+*/
 
   render() {
     return (
       <Router>
-        <Route exact path="/" render={() => 
+        <Route exact path="/" title = "CONESIEEE" render={() => 
           {return <div className = "contenedor-perfil"><Perfil/></div>}
         }>
         </Route>
--- conesieee/src/components/Perfil/Perfil.js
+++ conesieee/src/components/Perfil/Perfil.js
@@ -2,10 +2,45 @@ import React, { Component } from 'react'
 import './Perfil.css'
 export default class Perfil extends Component{
     
+    componentDidMount() {
+        document.title = "CONESIEEE";
+    }
 
     render(){
-        return(<h1>Si jala esto :v</h1>)
+        return(
+            <div class = "contenedor">
+                <div class ="titulo"><h1>¿De dónde nos visitas?</h1></div>
+                <div class="cards-list">
+                    <div class="card 1">
+                        <div class="card_image"> <img src="https://ik.imagekit.io/peu7i3asaiq/logo-usac-byempacharte_-iIJocM8FvB.png?updatedAt=1631578403795" /> </div>
+                        <div class="card_title title-black">
+                        <p>USAC</p>
+                        </div>
+                    </div>
+    
+                    <div class="card 2">
+                        <div class="card_image">
+                            <img src="https://ik.imagekit.io/peu7i3asaiq/depositphotos_320985390-stock-illustration-education-icon-vector-male-group_4rNzPIoIS.jpg?updatedAt=1631581351818" />
+                        </div>
+                        <div class="card_title title-black">
+                            <p>Otra Universidad</p>
+                        </div>
+                    </div>
+    
+                    <div class="card 3">
+                        <div class="card_image">
+                            <img src="https://ik.imagekit.io/peu7i3asaiq/unnamed_WK-768m8FY.jpg?updatedAt=1631581722991" />
+                        </div>
+                        <div class="card_title title-black">
+                            <p>Extranjero</p>
+                        </div>
+                    </div>
+                </div>
+                <img class = "imagen" src ="https://ik.imagekit.io/peu7i3asaiq/CONESIEE_NEGRO_0xBsj6Xhw.png?updatedAt=1631579312932" />
+            </div>
+          )
     }
 }
+
 
 
