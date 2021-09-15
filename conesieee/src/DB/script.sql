946e0b68fe64ef854183fc67a1ec8b0342f7bc02
César Alejandro Sosa Enríquez <60756401+Alejandro5852@users.noreply.github.com>
2021-09-14 23:26:02 -0600

    scritp again

--- conesieee/src/DB/script.sql
+++ conesieee/src/DB/script.sql
@@ -1,42 +1,96 @@
-CREATE TABLE Participante(
-    participante_id int NOT NULL AUTO_INCREMENT, 
-    nombres char(100) NOT NULL, 
-    apellidos char(100) NOT NULL,
-    identificacion char(100) NOT NULL,
-    correo char(100) NOT NULL,
-    carrera char(100) NULL,
-    facultad char(100) NOT NULL,
-    universidad char(100) NOT NULL,
-    PRIMARY KEY(participante_id)
-);
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE Conferencia(
-    conferencia_id int NOT NULL AUTO_INCREMENT,
-    tema char(250) NOT NULL,
-    expositor char(150) NOT NULL,
-    inicio datetime  NOT NULL,
-    fin datetime  NOT NULL,
-    PRIMARY KEY(conferencia_id)
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE TABLE Asignacion(
-    asignacion_id int NOT NULL AUTO_INCREMENT,
-    participante int NOT NULL,
-    conferencia int NOT NULL, 
-    fecha datetime  NOT NULL,
-    PRIMARY KEY(asignacion_id),
-    FOREIGN KEY (participante) REFERENCES Participante (participante_id),
-    FOREIGN KEY (conferencia) REFERENCES Conferencia (conferencia_id) 
-);
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema ieeedb
+-- -----------------------------------------------------
 
-CREATE TABLE Asistencia(
-    asistencia_id int NOT NULL AUTO_INCREMENT,
-    asignacion int NOT NULL,
-    nota int NOT NULL, 
-    fecha datetime  NOT NULL,
-    PRIMARY KEY(asistencia_id),
-    FOREIGN KEY (asignacion) REFERENCES Asignacion (asignacion_id),
-);
+-- -----------------------------------------------------
+-- Schema ieeedb
+-- -----------------------------------------------------
+DROP DATABASE ieeedb;
+
+CREATE SCHEMA IF NOT EXISTS `ieeedb` DEFAULT CHARACTER SET latin1 ;
+USE `ieeedb` ;
+
+-- -----------------------------------------------------
+-- Table `ieeedb`.`participante`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ieeedb`.`participante` (
+  `participante_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombres` VARCHAR(100) NOT NULL,
+  `apellidos` VARCHAR(100) NOT NULL,
+  `identificacion` VARCHAR(100) NOT NULL,
+  `correo` VARCHAR(100) NOT NULL,
+  `carrera` VARCHAR(100) NULL,
+  `facultad` VARCHAR(100) NULL,
+  `universidad` VARCHAR(100) NULL,
+   PRIMARY KEY (`participante_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `ieeedb`.`frente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ieeedb`.`conferencia` (
+  `conferencia_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `tema` VARCHAR(250) NOT NULL,
+  `expositor` VARCHAR(150) NOT NULL,
+  `inicio` DATETIME NOT NULL DEFAULT NOW(),
+  `fin` DATETIME NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (`conferencia_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `ieeedb`.`action_type`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ieeedb`.`asignacion` (
+  `asignacion_id` INT NOT NULL AUTO_INCREMENT,
+  `participante` int NOT NULL,
+  `conferencia` int NOT NULL,
+  `fecha` DATETIME NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (`asignacion_id`),
+  INDEX `fk_variable_participante_idx` (`participante` ASC),
+  INDEX `fk_variable_conferencia_idx` (`conferencia` ASC),
+CONSTRAINT `fk_variable_participante`
+    FOREIGN KEY (`participante`)
+    REFERENCES `ieeedb`.`participante` (`participante_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_variable_conferencia`
+    FOREIGN KEY (`conferencia`)
+    REFERENCES `ieeedb`.`conferencia` (`conferencia_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+  )
+ENGINE = InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS `ieeedb`.`asistencia` (
+  `asistencia_id` INT NOT NULL AUTO_INCREMENT,
+  `asignacion` int NOT NULL,
+  `nota` int NOT NULL,
+  `fecha` DATETIME NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (`asistencia_id`),
+  INDEX `fk_variable_asignacion_idx` (`asignacion` ASC),
+  CONSTRAINT `fk_variable_asistencia`
+    FOREIGN KEY (`asignacion`)
+    REFERENCES `ieeedb`.`asignacion` (`asignacion_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+  )
+ENGINE = InnoDB;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 
 
